cbe2f4cb804dc2a4de14fa33a0a0ae95b85ab4b4
Sai Aswin Raja <aswinsai45@gmail.com>
2025-03-20 11:12:30 +0530

    index clicked functionality

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,31 @@
-function ListGroup() {
-  const items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+import { useState } from "react";
 
+function ListGroup() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  let selectedIndex = 0;
+
+  //hook
+  useState;
   return (
     //{ items.map((item) => (<li>{item}</li>))} is a JSX expression that renders a list of items
+
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => (selectedIndex = index)}
+          >
+            {item}
+          </li>
         ))}
-        ;
       </ul>
     </>
   );
